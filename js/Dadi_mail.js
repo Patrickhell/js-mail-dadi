1fb83f0b51141662071eff38383df57a8009adac
Patrickhell <patrickhell19@yahoo.fr>
2023-05-17 16:13:23 +0200

    gioco dei dadi completato

---
+++ js/Dadi_mail.js
@@ -0,0 +1,25 @@
+ let user = 0 ;
+ let computer = 0 ;
+ shootNumber = S = 2;
+ let shoot = 1 ;
+ let score1;
+ let score2;
+ for (let x = 0 ; x < S ; x++) {
+ score1 = Math.floor(Math.random()*6 +1);
+ user = score1 ;
+ console.log("user il tuo punteggio è :" + score1);
+
+ score2 = Math.floor(Math.random()*6 +1);
+ computer = score2 ;
+ console.log("computer il tuo punteggio è :" + score2 );
+
+ 
+ if ( user < computer ) {
+    console.log("Awsome !! computer win!You score is :" + score2); 
+ }else if( user > computer ) {
+    console.log( "Awsome !! user win!You score is :" + score1); 
+ }else if (user = computer )   {
+    console.log("try again" ); 
+ }
+}
+ 
