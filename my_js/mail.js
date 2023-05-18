41fee277ad2bee562e1b1579bfa9feb3d58ef2f4
Patrickhell <patrickhell19@yahoo.fr>
2023-05-18 11:24:06 +0200

    correzione esercizio

--- my_js/mail.js
+++ my_js/mail.js
@@ -1,18 +1,22 @@
 
 
+const emailList = [ "patrickhell19@yahoo.fr", "patrick@yahoo.it", "jeanpatrick@yahoo.fr", "rodrigue@yahoo.it"];
+let user = prompt(" inserisci la tua Email");
+let email =  false;
 
-
-constVerification = document.querySelector("button");
-const emailList = [ "patrickhell19@yahoo.fr", "patrick@yahoo.it", "jeanpatrick@yahoo.fr"];
-let user = document.getElementById("email-user");
-
- constVerification.addEventListener( "click" , function() {
-    const outputElement = document.querySelector("ul");
     for (let x = 0 ; x < emailList.length ; x++) {
-        if ( user.value === emailList[x]) {
-            console.log("Your email is correct.Please continue");
-            outputElement.innerHTML = "Your email-address is correct.Please continue!!"
+       
+        if ( user ===  emailList[x]) {
+             email = true;
         } 
+        } 
+        
+        if (email ) {
+            console.log("continue");
+        } else {
+            console.log("try again");
         }
-        }
-        );
+            
+       
+        
+       
